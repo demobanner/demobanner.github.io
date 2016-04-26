8bcd7262cf57d53d5ea7494c561ab4fe821b11f3
kim.ha <kim.ha@sutrixmedia.com>
2016-04-26 17:10:13 +0700

    upload ICPO QX80

---
+++ AAC/Infiniti/ICPO_QX80/300x600_to_600x600_EN/logic.js
@@ -0,0 +1,147 @@
+
+var creative = {};
+
+/**
+ * Window onload handler.
+ */
+function preInit() {
+  setupDom();
+
+  if (Enabler.isInitialized()) {
+    init();
+  } else {
+    Enabler.addEventListener(
+      studio.events.StudioEvent.INIT,
+      init
+    );
+  }
+}
+
+/**
+ * Initializes the ad components
+ */
+function setupDom() {
+  creative.dom = {};
+  creative.dom.mainContainer = document.getElementById('main-container');
+  creative.dom.expandedExit = document.getElementById('expanded-exit');
+  creative.dom.expandedContent = document.getElementById('expanded-state');
+  creative.dom.collapsedExit = document.getElementById('collapsed-exit');
+  creative.dom.collapsedContent = document.getElementById('collapsed-state');
+  creative.dom.collapseButton = document.getElementById('collapse-button');
+  creative.dom.expandButton = document.getElementById('expand-button');
+  creative.dom.image0 = document.getElementById('banner-300x600');
+  creative.dom.image1 = document.getElementById('banner-600x600');
+}
+
+/**
+ * Ad initialisation.
+ */
+function init() {
+  Enabler.setExpandingPixelOffsets(0, 0, 600, 600);
+  Enabler.setStartExpanded(false);
+
+  addListeners();
+
+  // Polite loading
+  if (Enabler.isVisible()) {
+    show();
+  }
+  else {
+    Enabler.addEventListener(studio.events.StudioEvent.VISIBLE, show);
+  }
+}
+
+/**
+ * Adds appropriate listeners at initialization time
+ */
+function addListeners() {
+  Enabler.addEventListener(studio.events.StudioEvent.EXPAND_START, expandStartHandler);
+  Enabler.addEventListener(studio.events.StudioEvent.EXPAND_FINISH, expandFinishHandler);
+  Enabler.addEventListener(studio.events.StudioEvent.COLLAPSE_START, collapseStartHandler);
+  Enabler.addEventListener(studio.events.StudioEvent.COLLAPSE_FINISH, collapseFinishHandler);
+  creative.dom.expandButton.addEventListener('click', onExpandHandler, false);
+  creative.dom.collapseButton.addEventListener('click', onCollapseClickHandler, false);
+  creative.dom.expandedExit.addEventListener('click', exitClickHandler);
+  creative.dom.collapsedExit.addEventListener('click', collapsedExitClickHandler);
+}
+
+/**
+ *  Shows the ad.
+ */
+function show() {
+  creative.dom.expandedContent.style.display = 'none';
+  creative.dom.expandedExit.style.display = 'none';
+  creative.dom.collapseButton.style.display = 'none';
+
+  creative.dom.collapsedContent.style.display = 'block';
+  creative.dom.collapsedExit.style.display = 'block';
+  creative.dom.expandButton.style.display = 'block';
+  creative.dom.image0.style.visibility  = 'visible';
+  creative.dom.image1.style.visibility  = 'visible';
+}
+
+// ---------------------------------------------------------------------------------
+// MAIN
+// ---------------------------------------------------------------------------------
+
+
+function expandStartHandler() {
+  // Show expanded content.
+  creative.dom.expandedContent.style.display = 'block';
+  creative.dom.expandedExit.style.display = 'block';
+  creative.dom.collapseButton.style.display = 'block';
+  creative.dom.collapsedContent.style.display = 'none';
+  creative.dom.collapsedExit.style.display = 'none';
+  creative.dom.expandButton.style.display = 'none';
+
+  Enabler.finishExpand();
+}
+
+function expandFinishHandler() {
+  creative.isExpanded = true;
+}
+
+function collapseStartHandler() {
+  // Perform collapse animation.
+  creative.dom.expandedContent.style.display = 'none';
+  creative.dom.expandedExit.style.display = 'none';
+  creative.dom.collapseButton.style.display = 'none';
+  creative.dom.collapsedContent.style.display = 'block';
+  creative.dom.collapsedExit.style.display = 'block';
+  creative.dom.expandButton.style.display = 'block';
+
+  // When animation finished must call
+  Enabler.finishCollapse();
+}
+
+function collapseFinishHandler() {
+  creative.isExpanded = false;
+}
+
+function onCollapseClickHandler(){
+  Enabler.requestCollapse();
+  Enabler.stopTimer('Panel Expansion');
+  restart_300x600();
+}
+
+function onExpandHandler(){
+  Enabler.requestExpand();
+  Enabler.startTimer('Panel Expansion');
+  restart_600x600();
+}
+
+function exitClickHandler() {
+  Enabler.requestCollapse();
+  Enabler.stopTimer('Panel Expansion');
+  Enabler.exit('BackgroundExit');
+}
+
+function collapsedExitClickHandler() {
+  Enabler.exit('CollapsedExit');
+}
+
+
+/**
+ *  Main onload handler
+ */
+window.addEventListener('load', preInit);
