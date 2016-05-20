1372260367361edb8c14dfcff4a77e35289d7c8c
kim.ha <kim.ha@sutrixmedia.com>
2016-05-20 15:52:46 +0700

    upload Digitas Smart banners

---
+++ Digitas/NissanAlwaysOn_Belux/336x280/config.js
@@ -0,0 +1,134 @@
+define(
+	   {
+  "ITEMS": [
+    {
+      "id": "contentBanner",
+      "type": "adkit-html",
+      "svKey": "content"
+    }
+  ],
+  "SV": {
+    "svData": [
+      {
+        "svKey": "f1_title",
+        "svType": "String",
+        "value": "L’INNOVATION EST<br>PLUS ACCESSIBLE<br>QUE JAMAIS"
+      },
+      {
+        "svKey": "f1_title_style",
+        "svType": "String",
+        "value": "black,18px"
+      },
+      {
+        "svKey": "f1_image",
+        "svType": "String",
+        "value": "images/f1_image.png"
+      },
+      {
+        "svKey": "f2_title",
+        "svType": "String",
+        "value": "NISSAN NOTE"
+      },
+      {
+        "svKey": "f2_title_style",
+        "svType": "String",
+        "value": "black,25px"
+      },
+      {
+        "svKey": "f2_subtitle1",
+        "svType": "String",
+        "value": "avec Techno Deal"
+      },
+      {
+        "svKey": "f2_subtitle1_style",
+        "svType": "String",
+        "value": "black,17px"
+      },
+      {
+        "svKey": "f2_subtitle2",
+        "svType": "String",
+        "value": "jusqu’à 4.365 €<br>d’avantages clients"
+      },
+      {
+        "svKey": "f2_subtitle2_style",
+        "svType": "String",
+        "value": "black,17px"
+      },
+      {
+        "svKey": "logo",
+        "svType": "String",
+        "value": "images/logo.png"
+      },
+      {
+        "svKey": "background",
+        "svType": "String",
+        "value": "images/background.jpg"
+      },
+      {
+        "svKey": "packshot",
+        "svType": "String",
+        "value": "images/packshot_note.png"
+      },
+      {
+        "svKey": "headlights",
+        "svType": "String",
+        "value": "images/headlights_note.png"
+      },
+      {
+        "svKey": "cta_icon",
+        "svType": "String",
+        "value": "images/icon_arrow.png"
+      },
+      {
+        "svKey": "cta_icon_animate",
+        "svType": "String",
+        "value": "images/icon_arrow.png"
+      },
+      {
+        "svKey": "cta_icon_style",
+        "svType": "String",
+        "value": "24px,33px,5px,0px"
+      },
+      {
+        "svKey": "cta_text",
+        "svType": "String",
+        "value": "C’EST ICI"
+      },
+      {
+        "svKey": "cta_text_style",
+        "svType": "String",
+        "value": "13px,33px,15px"
+      },
+      {
+        "svKey": "cta_wrapper_style",
+        "svType": "String",
+        "value": "110px,33px,113px,233px"
+      },
+      {
+        "svKey": "cta_highlight",
+        "svType": "String",
+        "value": "images/cta_highlight.png"
+      },
+      {
+        "svKey": "cta_highlight_style",
+        "svType": "String",
+        "value": "71px,33px,150px"
+      },
+      {
+        "svKey": "Standalone",
+        "svType": "String",
+        "value": "images/standalone.jpg"
+      },
+      {
+        "svKey": "Standalone_status",
+        "svType": "String",
+        "value": "false"
+      },
+      {
+        "svKey": "border",
+        "svType": "String",
+        "value": "true"
+      }
+    ]
+  }
+});
