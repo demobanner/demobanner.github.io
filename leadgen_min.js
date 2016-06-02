f1f1075e6cedf37c810222df31b2e58991e580fd
kim.ha <kim.ha@sutrixmedia.com>
2016-06-02 14:24:06 +0700

    update NCPO Ramadan

--- leadgen_min.js
+++ leadgen_min.js
@@ -7,17 +7,17 @@ l.prototype.createdCallback=function(){
   this.f=e(this.getAttribute("include_name"));
   this.c=e(this.getAttribute("include_email"));
   this.g=e(this.getAttribute("include_phone"));
-  this.b=e(this.getAttribute("include_address"));
+  this.b=e(this.getAttribute("include_model"));
   this.v=this.getAttribute("placeholder_name");
   this.u=this.getAttribute("placeholder_email");
-  this.w=this.getAttribute("placeholder_phone");this.o=this.getAttribute("placeholder_address");
+  this.w=this.getAttribute("placeholder_phone");this.o=this.getAttribute("placeholder_model");
 
   this.A=this.getAttribute("placeholder_submit");
   this.s=this.getAttribute("placeholder_confirmation");
   this.j=d(this,this.j,"leadgen-name","text",this.v,this.f);
   this.i=d(this,this.i,"leadgen-email","email",this.u,this.c);
   this.l=d(this,this.l,"leadgen-phone","tel",this.w,this.g);
-  this.h=d(this,this.h,"leadgen-address","text",this.o,this.b);
+  this.h=d(this,this.h,"leadgen-model","text",this.o,this.b);
   var b=this.a,a=this.A,b=document.createElement("button");
   b.id="leadgen-submit";
   b.textContent=a;
@@ -37,7 +37,7 @@ l.prototype.attributeChangedCallback=function(b){
       break;
     case "include_phone":this.g=e(a);
       break;
-    case "include_address":this.b=e(a);
+    case "include_model":this.b=e(a);
       break;
     case "placeholder_name":this.v=a;
       break;
@@ -45,7 +45,7 @@ l.prototype.attributeChangedCallback=function(b){
       break;
     case "placeholder_phone":this.w=a;
       break;
-    case "placeholder_address":this.o=a;
+    case "placeholder_model":this.o=a;
       break;
     case "placeholder_submit":this.A=a;
       break;
@@ -54,7 +54,7 @@ l.prototype.attributeChangedCallback=function(b){
 };
 l.prototype.B=function(b){
   b.preventDefault();
-  f("name="+encodeURIComponent(this.j.value)+"&email="+encodeURIComponent(this.i.value)+"&phone="+encodeURIComponent(this.l.value)+"&address="+encodeURIComponent(this.h.value),this.m);
+  f("name="+encodeURIComponent(this.j.value)+"&email="+encodeURIComponent(this.i.value)+"&phone="+encodeURIComponent(this.l.value)+"&model="+encodeURIComponent(this.h.value),this.m);
   this.a.disabled=!0;
   this.a.textContent=this.s;
   b=document.createEvent("Event");
