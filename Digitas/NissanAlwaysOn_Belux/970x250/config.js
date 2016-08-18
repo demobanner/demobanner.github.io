15f4f383362f4174a4eca183bf3b5d805585232c
kim.ha <kim.ha@sutrixmedia.com>
2016-08-18 15:49:39 +0700

    upload Digitas Always On

---
+++ Digitas/NissanAlwaysOn_Belux/970x250/config.js
@@ -0,0 +1,184 @@
+define(
+	   {
+  "ITEMS": [
+    {
+      "id": "contentBanner",
+      "type": "adkit-html",
+      "svKey": "content"
+    }
+  ],
+  "SV": {
+    "svData": [
+      {
+        "svKey": "f1_title",
+        "svType": "String",
+        "value": "INNOVATIE NU<br>BEREIKBAARDER<br>DAN OOIT"
+      },
+      {
+        "svKey": "f1_title_style",
+        "svType": "String",
+        "value": "black,31px"
+      },
+      {
+        "svKey": "f1_image",
+        "svType": "Image",
+        "value": 1
+      },
+      {
+        "svKey": "f2_title",
+        "svType": "String",
+        "value": ""
+      },
+      {
+        "svKey": "f2_title_style",
+        "svType": "String",
+        "value": "black,25px"
+      },
+      {
+        "svKey": "f2_subtitle",
+        "svType": "String",
+        "value": "DANKZIJ DE<br><span>TECHNO DEALS</span>"
+      },
+      {
+        "svKey": "f2_subtitle_style",
+        "svType": "String",
+        "value": "black,32px, 42, #c71444"
+      },
+      {
+        "svKey": "f3_title",
+        "svType": "String",
+        "value": "NISSAN NOTE"
+      },
+      {
+        "svKey": "f3_title_style",
+        "svType": "String",
+        "value": "black,40px"
+      },
+      {
+        "svKey": "f3_subtitle",
+        "svType": "String",
+        "value": "tot €4.650 voordeel"
+      },
+      {
+        "svKey": "f3_subtitle_style",
+        "svType": "String",
+        "value": "black,23px, 90, #c71444"
+      },
+      {
+        "svKey": "logo",
+        "svType": "Image",
+        "value": 2
+      },
+      {
+        "svKey": "background",
+        "svType": "Image",
+        "value": 3
+      },
+      {
+        "svKey": "packshot",
+        "svType": "Image",
+        "value": 4
+      },
+      {
+        "svKey": "headlights",
+        "svType": "Image",
+        "value": 5
+      },
+      {
+        "svKey": "cta_icon",
+        "svType": "Image",
+        "value": 6
+      },
+      {
+        "svKey": "cta_icon_animate",
+        "svType": "Image",
+        "value": 7
+      },
+      {
+        "svKey": "cta_icon_style",
+        "svType": "String",
+        "value": "24px,33px,20px,0px"
+      },
+      {
+        "svKey": "cta_text",
+        "svType": "String",
+        "value": "ONTDEK HIER"
+      },
+      {
+        "svKey": "cta_text_style",
+        "svType": "String",
+        "value": "21px,44px,29px"
+      },
+      {
+        "svKey": "cta_wrapper_style",
+        "svType": "String",
+        "value": "227px,43px,556px,155px"
+      },
+      {
+        "svKey": "cta_highlight",
+        "svType": "Image",
+        "value": 8
+      },
+      {
+        "svKey": "cta_highlight_style",
+        "svType": "String",
+        "value": "71px,43px,215px"
+      },
+      {
+        "svKey": "Standalone",
+        "svType": "Image",
+        "value": 9
+      },
+      {
+        "svKey": "Standalone_status",
+        "svType": "String",
+        "value": "false"
+      },
+      {
+        "svKey": "border",
+        "svType": "String",
+        "value": "true"
+      }
+    ]
+  },
+
+  "AdditionalAssets": [
+    {
+        "OrdinalNumber": 1,
+        "FileName": "f1_image_NL.png"
+    },
+    {
+        "OrdinalNumber": 2,
+        "FileName": "logo.png"
+    },
+    {
+        "OrdinalNumber": 3,
+        "FileName": "background.jpg"
+    },
+    {
+        "OrdinalNumber": 4,
+        "FileName": "packshot_note.png"
+    },
+    {
+        "OrdinalNumber": 5,
+        "FileName": "headlights_note.png"
+    },
+    {
+        "OrdinalNumber": 6,
+        "FileName": "icon_arrow.png"
+    },
+    {
+        "OrdinalNumber": 7,
+        "FileName": "icon_arrow_animate.png"
+    },
+    {
+        "OrdinalNumber": 8,
+        "FileName": "cta_highlight.png"
+    },
+    {
+        "OrdinalNumber": 9,
+        "FileName": "standalone.jpg"
+    },
+  ]
+
+});
